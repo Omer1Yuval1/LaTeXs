3a5f027db9114a84236984a165ebfb43ac1dda54
Omer Yuval <omer1yuval1@gmail.com>
2022-05-25 16:12:53 +0100

    Traslated the test_cases file to js, and started to translate index.js and parse_formula.js.

---
+++ functions/validation/test_cases.js
@@ -0,0 +1,104 @@
+function str = test_cases(n) {
+	
+	var str = [];
+	var i = -1;
+	
+	// Simple cases
+	str[++i] = 'x + y + z';
+	str[++i] = '2xy + zw';
+	str[++i] = 'x^2';
+	str[++i] = '3*x^2';
+	str[++i] = 'z*x*y';
+	
+	// Indices
+	str[++i] = '2x_3 + 4';
+	str[++i] = '2x_3 + x*y*z';
+	str[++i] = 'b5xa + 2x_3 + y*a*h + w';
+	
+	// Minus sign
+	str[++i] = 'x * y - z';
+	str[++i] = '2x - x*y*z';
+	str[++i] = 'x*y*z + 2x^3';
+	str[++i] = '2x_3 - x*y*z';
+	str[++i] = '-y*z + w'; // Minus as first character.
+	
+	// Parentheses
+	str[++i] = '(y+z)*x';
+	str[++i] = 'x + (y*z)';
+	str[++i] = 'x * (y + z + w)';
+	str[++i] = '(y*z + w)';
+	str[++i] = '(y + z*w)';
+	str[++i] = 'x + (y*z + w)';
+	str[++i] = 'x + (w + y*z)';
+	str[++i] = 'x + (w^2 + y*z)';
+	
+	// Parentheses and minus sign
+	str[++i] = 'x + (y*z^2 - w)';
+	str[++i] = 'x - (y+z)';
+	str[++i] = 'x^(y+z)'; // This is wrong syntax in LaTeX. Should be 'x^{y+z}'.
+	str[++i] = 'x^{y+z}';
+	
+	// Parentheses and power
+	str[++i] = '1 + (x*y)^2';
+	
+	// LaTeX commands
+	str[++i] = '\frac{x}{y}*b';
+	str[++i] = '\frac{x+2*w}{y^3}';
+	str[++i] = '\sqrt{x}';
+	str[++i] = '\sqrt[4]{x}';
+	
+	// Multi-digit numbers and decimals
+	str[++i] = 'x + 32.4';
+	str[++i] = 'x^23';
+	str[++i] = 'x^2*3';
+	str[++i] = '\frac{y^x}{x+b} + z^{\sqrt[3]{y}}';
+	str[++i] = 'z^{\sqrt[3]{w}} + \frac{w^r}{r+m}';
+	
+	// Middle and forward operations
+	str[++i] = 'a \in b';
+	str[++i] = '{a^3} \in {b^2} + c';
+	str[++i] = '{a^3} \in \frac[++i][++i]';
+	
+	// Combination of index and power operators
+	str[++i] = 'a_b^c';
+	str[++i] = 'a^b_c';
+	
+	// Sorting of numbers according to their numeric value
+	str[++i] = 'z + 3 + y + 1 + x + 2';
+	
+	// Summation, integration and limits
+	str[++i] = '\sum_{i+1}^{N+1}{i^2}';
+	str[++i] = '\int_{i+1}^{N+1}{i^2}';
+	str[++i] = '\sum_i^N';
+	str[++i] = '\sum_i^N [++i]';
+	str[++i] = '\lim_{i \to \infty} [++i]';
+	str[++i] = 'a_{i+1}[++i]';
+	
+	// Comparisons
+	str[++i] = 'x^2 + y = w + z^3';
+	str[++i] = 'x + 2 = y = z';
+	str[++i] = '2 + x^2 > y + 2';
+	str[++i] = 'x > y < z';
+	str[++i] = 'x > y';
+	
+	// Trigonometric functions
+	str[++i] = '\tan(x)(y)'; // A * sign is added during parsing.
+	str[++i] = '\sin^{-1}(x+6)'; // Inverse sine function is replaced by arcsin.
+	
+	// Comparison inside parentheses
+	str[++i] = '\sum_{i=0}^{N}[++i]';
+	str[++i] = '\cos(x=0)';
+	
+	// Unnecessary arguments
+	str[++i] = '\frac{x}{y}{b}';
+	str[++i] = '\sqrt[3]{x}{y}{b}';
+	str[++i] = '\nabla(x+4)(5)';
+	str[++i] = '\lim_{i \to \infty} [++i]{x}';
+	
+	
+	if(arguments.length == 1) {
+		str = str[n];
+	}
+	
+	return str;
+}
---
+++ index.js
@@ -0,0 +1,64 @@
+function index(test_ids,rep_vars,plot_01) {
+	
+	// cd(fileparts(which(mfilename)));
+	// addpath(genpath(pwd));
+	
+	if(arguments.length == 0 || !test_ids) {
+		var test_ids = 1;
+	}
+	
+	if(arguments.length < 3 || !rep_vars) {
+		var rep_vars = 1;
+	}
+	
+	if(!isNaN(test_ids)) { // If it's a number.
+		var str = test_cases(test_ids); // Load test cases.
+	} else { // If a formula is given as an input string.
+		var str[0] = test_ids;
+	}
+	
+	for(let i=0; i< str.length; i++) // i=1:length(str) {
+		// disp(str{i});
+		// disp(test_ids(i));
+		
+		let S = {'id': [], 'str': [], 'parent_id': []);
+		
+		str_i = preprocess_input(str[i]);
+		
+		[op_ind,op_priority,op_type] = op2ind('=',1);
+		id = 1;
+		S(id).id = id;
+		S(id).parent_id = 0; // Top level.
+		S(id).operator = op2ind('=',1);
+		S(id).type = op_type;
+		S(id).str = str_i;
+		
+		S = parse_formula(S,1,id,op_ind,op_priority,op_type,[]);
+		
+		S = postprocess_tree(S);
+		
+		S = sort_tree(S,0,0);
+		
+		if(rep_vars) {
+			S = replace_var_names(S);
+		}
+		
+		if(nargin < 3 || plot_01) {
+			if(i == 1) {
+				figure;
+			}
+			
+			// set(gcf,'WindowState','Maximized');
+			plot_AST(S,str[i]);
+			set(gcf,'Name',num2str(test_ids(i)));
+			// assignin('base','S',S);
+			
+			if(str.length > 1) {
+				waitforbuttonpress;
+				clf;
+			}
+		}
+	}
+	
+	return S;
+}
