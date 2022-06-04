9e01654af91ed27fc7877b08c88470d5b36528a6
Omer Yuval <omer1yuval1@gmail.com>
2022-06-04 15:01:30 +0100

    Added tree simplification for several cases where {} has only one child, depending on the parent of the {}. Excluded cases include operators that use {} for input in combination with other operators (e.g., ^, _, []).

--- functions/simplify_tree.js
+++ functions/simplify_tree.js
@@ -15,30 +15,49 @@ function simplify_tree(S) {
 	
 	
 	// Rules
+		// ***** Can't remove operator elements with a minus sign.
 		// If a + has only one child, remove the +. This will automatucally account for cases with a comparison operator that has two or more + children. See test case 51+52.
 		// If an = has only one child, remove the =. Sometimes the = will be in the form {=} or (=). See test case 42.
 			// But perhaps in some cases the () are important, such as in cos(x+1). Or maybe not.
 			// For some operator types, [] and {} (as well as ^ and _) refer to specific arguments. See test case 58.
 		// Remove () (index = 10) if it has one child. See test case 53 + 19-24.
+		// Remove {} only if they have one child, but only if their parent is:
+			// frac (index = 17). See test case 27 + 35.
+			// type=0 (middle operators with no special inputs). See test case 35.
+			// type=1 (complex middle operators - comparison). See test case 37 + 60.
 		
-		// TODO:
-			// ***** Can't remove elements with a minus sign.
-			// Remove {} only if they have one child, but only if their parent is:
-				// frac (index = 17). See test case 35.
-				// type=0 (middle operators with no special inputs). See test case 35.
-				// type=1 (complex middle operators - comparison). See test case 37 and 60.
-			// If {} or () has more than one direct child, replace it with =.
-			// remove * if its parent is also *.
+	// TODO:
+		// If {} or () has more than one direct child, replace it with =.
+		// remove * if its parent is also *.
 	
 	var toRemove = [];
 	for(let i=0; i<S.length; i++) {
-		if(S[i].operator == 3 || S[i].operator == 4 || S[i].operator == 10) { // '+' || '=' || ().
-			let Cn = get_children(S,S[i].id); // Cn is an array of child indices (positions, not ids) in S.
-			if(Cn.length <= 1) { // If it has up to 1 (direct) child.
-				for(let j=0; j<Cn.length; j++) { // Update the parent_id of the children to be the parent of their parent.
-					S[Cn[j]].parent_id = S[i].parent_id;
+		if(S[i].operator == 3 || S[i].operator == 4 || S[i].operator == 10) { // '+' (3) || '=' (4) || () (10).
+			if(S[i].sign != '-') { // If it does not have a minus sign.
+				let Cn = get_children(S,S[i].id); // Cn is an array of child indices (positions, not ids) in S.
+				if(Cn.length <= 1) { // If it has up to 1 (direct) child.
+					for(let j=0; j<Cn.length; j++) { // Update the parent_id of the children to be the parent of their parent.
+						S[Cn[j]].parent_id = S[i].parent_id;
+					}
+					toRemove.push(i); // Mark the parent for removal.
 				}
-				toRemove.push(i); // Mark the parent for removal.
+			}
+		} else if(S[i].operator == 9 && S[i].sign != '-') { // {} without a minus sign.
+			let Cn = get_children(S,S[i].id);
+			if(Cn.length <= 1) { // If it has up to 1 (direct) child.
+				for(let ii=0; ii<S.length; ii++) { // First find the parent of the {}.
+					if(S[i].parent_id == S[ii].id) {
+						if(S[ii].operator == 16 || S[ii].type == 0 || S[ii].type == 1) { // If the parent of the {} is \frac || type == 0 (e.g., ^) || type == 1 (.e.,g \in).
+							for(let j=0; j<Cn.length; j++) { // Update the parent_id of the children to be the parent of their parent.
+								S[Cn[j]].parent_id = S[i].parent_id;
+							}
+							toRemove.push(i); // Mark the parent for removal.
+						}
+						break; // There is only parent, so break once it is found.
+					}
+				}
+			} else { // If it has 2+ children.
+				
 			}
 		}
 	}
